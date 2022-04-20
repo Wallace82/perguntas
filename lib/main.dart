9722a4b9097a22fe6043dc85e1caf81d5c2943d3
wallace <gomesw@gmail.com>
2022-04-20 11:57:57 -0300

    3 - Usando pela primeira vez o evento onpresed do RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,43 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
+
+
 class PerguntaApp extends StatelessWidget{
+
+  void responder(){
+    print('Pergunta repondida!');
+  }
 
   @override
   Widget build(BuildContext context){
+
+    final perguntas  = ['Qual é sua cor favorita?', 'Qual é seu animal favorito?'];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Óla Flutter'),
+        body: Column(
+           children:<Widget> [
+             Text(perguntas[0]),
+             RaisedButton(
+                 child: Text('Resposta 1') ,
+                 onPressed: (){
+                   print('Resposta 1 respondida!');
+                 }
+             ),
+             RaisedButton(
+                 child: Text('Resposta 2') ,
+                 onPressed: ()=> print('Resposta 2 respondida!')
+             ),
+             RaisedButton(
+                 child: Text('Resposta 3') ,
+                 onPressed: responder
+             ),
+           ],
+        )
       ),
     );
   }
